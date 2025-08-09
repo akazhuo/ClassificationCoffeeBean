593fad5a18f38bd89ed84b0abdc02698ac4521a6
akazhuo <akazhuo@qq.com>
2025-08-09 21:38:55 +0800

    feat: 图表组件

--- src/components/MyChart.vue
+++ src/components/MyChart.vue
@@ -1,7 +1,3 @@
-<template>
-  <v-chart class="chart" :option="option" />
-</template>
-
 <script setup lang="ts">
 import { use } from "echarts/core";
 import { CanvasRenderer } from "echarts/renderers";
@@ -12,7 +8,7 @@ import {
   LegendComponent
 } from "echarts/components";
 import VChart, { THEME_KEY } from "vue-echarts";
-import { ref, provide } from "vue";
+import { ref, provide, computed, onUpdated } from 'vue'
 import { store } from '../stores/classification.ts';
 
 use([
@@ -23,11 +19,32 @@ use([
   LegendComponent
 ]);
 
-provide(THEME_KEY, "dark");
+provide(THEME_KEY, "light");
+
+const CATEGORIES = ['Dark', 'Green', 'Light', 'Medium']
+const chartRef = ref('')
+const coffeeBeanStore = store()
+// 分类预测数组 ['0.00', '0.02', '99.87', '0.11']
+const predictionArray = computed(() => coffeeBeanStore.predictionArray)
+// 订阅预测数据更新
+coffeeBeanStore.$subscribe((mutation, state) => {
+  chartRef.value.setOption({
+    series: [
+      {
+        data: [
+          { value: state.predictionArray[0], name: CATEGORIES[0] },
+          { value: state.predictionArray[1], name: CATEGORIES[1] },
+          { value: state.predictionArray[2], name: CATEGORIES[2] },
+          { value: state.predictionArray[3], name: CATEGORIES[3] },
+        ]
+      }
+    ]
+  })
+})
 
 const option = ref({
   title: {
-    text: "Traffic Sources",
+    text: "咖啡豆烘焙度",
     left: "center"
   },
   tooltip: {
@@ -37,20 +54,19 @@ const option = ref({
   legend: {
     orient: "vertical",
     left: "left",
-    data: ["Direct", "Email", "Ad Networks", "Video Ads", "Search Engines"]
+    data: CATEGORIES,
   },
   series: [
     {
-      name: "Traffic Sources",
+      name: "烘焙度",
       type: "pie",
       radius: "55%",
       center: ["50%", "60%"],
       data: [
-        { value: 335, name: "Direct" },
-        { value: 310, name: "Email" },
-        { value: 234, name: "Ad Networks" },
-        { value: 135, name: "Video Ads" },
-        { value: 1548, name: "Search Engines" }
+        { value: predictionArray.value[0], name: CATEGORIES[0] },
+        { value: predictionArray.value[1], name: CATEGORIES[1] },
+        { value: predictionArray.value[2], name: CATEGORIES[2] },
+        { value: predictionArray.value[3], name: CATEGORIES[3] },
       ],
       emphasis: {
         itemStyle: {
@@ -64,6 +80,13 @@ const option = ref({
 });
 </script>
 
-<style scoped>
+<template>
+  <v-chart ref="chartRef" class="my-chart" :option="option" />
+</template>
 
+
+<style scoped>
+.my-chart {
+  height: 400px;
+}
 </style>
